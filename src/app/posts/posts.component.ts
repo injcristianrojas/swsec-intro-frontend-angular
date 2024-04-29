79447cf2c4a843eed037c17ffeffe0ef2ca8f343
Cristián Rojas <injcristianrojas@gmail.com>
2024-04-29 17:52:56 -0400

    Message functionality

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-posts',
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
 
-  constructor() { }
+  posts: any;
+  postForm = new FormGroup({
+    mensaje: new FormControl('')
+  });
+  invalid: boolean = false;
+
+  constructor(private api: ApiService) { }
 
   ngOnInit(): void {
+    this.getPosts();
+  }
+
+  getPosts(): void {
+    this.api.getPosts().subscribe(
+      data => {
+        this.posts = data;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  insertPost(message: string): void {
+    this.api.insertPost(message).subscribe(
+      data => {
+        this.getPosts();
+        this.postForm.reset();
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  onSubmit(): void {
+    this.invalid = this.postForm.status == 'INVALID';
+    if (!this.invalid)
+      this.insertPost(this.postForm.value.mensaje);
   }
 
 }
